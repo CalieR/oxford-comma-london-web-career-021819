f77e63647479aa77c513f992f505b16f8de43e7a
Calie Rushton <calielrushton@gmail.com>
2019-01-02 16:47:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,10 @@ def oxford_comma(array)
   if array.length == 1
     array.join
   elsif array.length == 2
-    array.join(" and ")  
+    array.join(" and ")
+  else
+    last_word = array.pop
+    first_part = array.join(", ")
+    return first_part + ", and" + last_word  
   end
 end
